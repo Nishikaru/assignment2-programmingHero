c18d1fc9d0ab6630472e6c027a7f1c2df7c0983b
Nishikaru <mdsajed001@gamil.com>
2022-07-20 05:14:32 +0600

    some fixes

--- test/welcome.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    if (isset($_POST['login'])) {
-        $username = $_POST['username'];
-        $pass = $_POST['password'];
-        echo 'Welcome <b>' .$username. ' </b> your password is <b>'.$pass. '</b>';
-    }
-?>
